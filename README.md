4cb87b7616a3e9a3839faf887966a8a0c689dc43
Ujjwal Dwivedi <ujjwaldwivedi567@gmail.com>
2024-12-02 22:20:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### shipment price prediction
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="shipment",
+    version='0.0.1',
+    author='ujjwal',
+    author_email='ujjwaldwivedi567@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
